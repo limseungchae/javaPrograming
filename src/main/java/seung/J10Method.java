c349c160bde7df816e65df61baa4924bb98b141a
limseungchae <tmdco12@naver.com>
2023-04-07 18:24:16 +0900

    클래스 기반 성적처리 프로그램 1

--- src/main/java/seung/J10Method.java
+++ src/main/java/seung/J10Method.java
@@ -1,12 +1,12 @@
 package seung;
 
+import javax.security.sasl.SaslClient;
 import java.util.Scanner;
 
 public class J10Method {
     public static void main(String[] args) {
-
         // 메서드
-        // 특정 작업(코드블록)을 여러번 반복해야 하는 경우
+        // 특정 작업(코드블록)을 여러 번 반복해야 하는 경우
         // 해당 작업을 재사용 가능한 구조로 만들때 사용
         // 즉, 어떤 작업을 수행하기 위해
         // 필요한 코드들의 집합을 의미하고,
@@ -16,22 +16,21 @@ public class J10Method {
         sayHello();
 
         sayHello2("Java");
+        sayHello2("HTML");
 
-        sayHello2("HTML ");
-
-        System.out.println(sayHello3("JavaScript"));
+        System.out.println( sayHello3("JavaScript") );
 
         int year = 2020;
-        System.out.println(computeLeapYear(year));
+        System.out.println( computeLeapYear(year) );
 
         String time = "17:17:17";
-        System.out.println(showAMPM(time));
+        System.out.println( showAMPM(time) );
 
-        System.out.println(showStarPyramid(5));
-        System.out.println(showStarPyramid(7));
-        System.out.println(showStarPyramid(3));
+        System.out.println( showStarPyramid(5) );
+        System.out.println( showStarPyramid(7) );
+        System.out.println( showStarPyramid(3) );
 
-        // ex) 성적 처리 프로그램 v4
+        // ex) 성적 처리프로그램 v4
         String[] names = new String[3];
         int[] kors = new int[3];
         int[] engs = new int[3];
@@ -41,80 +40,74 @@ public class J10Method {
         double[] avgs = new double[3];
         char[] grds = new char[3];
 
-        readSungJuk(names, kors, engs, mats);
-        computeSungJuk(kors, engs, mats, tots, avgs, grds);
-        printSungJuk(names, kors, engs, mats, tots, avgs, grds);
+        readSungJuk(names,kors,engs,mats);
+        computeSungJuk(kors,engs,mats, tots,avgs,grds);
+        printSungJuk(names,kors,engs,mats, tots,avgs,grds);
 
-        // 만일, 2과목이 추가된다면?
+        // 만일, 2 과목이 추가된다면?
         // 메소드 호출부 수정, 메서드 선언부 수정
         // 과목 입력 수정, 성적처리 수정, 성적 출력 수정
 
     }
 
     // 인삿말 v1 - 메서드 정의
-    // 접근 제한자 static 반환유형 메서드명(매개변수목록)
+    // 접근제한자 static 반환유형 메서드명(매개변수목록)
     public static void sayHello() {
         System.out.println("Hello, World!!");
-    }
-
-    ;
+    };
 
     // 인삿말 v2 - 메서드 매개변수 사용
     public static void sayHello2(String msg) {
         System.out.printf("Hello, %s!!\n", msg);
-    }
+    };
 
-    ;
-
-    // 인삿말 v3 - 변환형 메서드 선언
+    // 인삿말 v3 - 반환형 메서드 선언
     public static String sayHello3(String msg) {
         return String.format("Hello, %s!!\n", msg);
-    }
-
-    ;
+    };
 
     // 년도를 입력하면 윤년여부를 계산해서 출력
     // 윤년여부를 계산하는 반환형 메서드 : computeLeapYear
     public static String computeLeapYear(int year) {
         String result = "평년입니다";
 
-        if (((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
             result = "윤년입니다";
 
         return String.format("%d 년은 %s\n", year, result);
-
     }
 
     // 시분초(17:17:17)를 입력받아
     // 오전, 오후 구분하여 출력 : showAMPM
     public static String showAMPM(String time) {
         String result = "오전입니다";
-        int hour = Integer.parseInt((time.substring(0, 2)));
+        int hour = Integer.parseInt(time.charAt(0) + "" + time.charAt(1));
 
         if (hour >= 12) result = "오후입니다";
 
-        return String.format("%d시는 %s\n", hour, result);
+        return String.format("%d 시는 %s\n", hour, result);
     }
 
-    // 총수를 입력받아
-    // 별 * 피라미드 출력 : showStarPyramid
+
+    // 층 수를 입력받아
+    // 별* 피라미드 출력 : showStarPyramid
     public static String showStarPyramid(int floor) {
         String result = "";
 
-        for (int i = 1; i <= floor; i++) { // 행
-            for (int j = 1; j <= i; j++) { // 열
+        for (int i = 1; i <= floor; i++) {  // 행
+            for (int j = 1; j <= i; j++) {  // 열
                 result += "*";
             }
             result += "\n";
         }
+
         return result;
     }
 
-
-    // ex) 성적 처리 프로그램 v4
+    // ex) 성적 처리프로그램 v4
     // 3명의 학생에 대해 이름, 국어, 영어, 수학을 입력받아
     // 총점, 평균, 학점을 계산한 뒤 결과 출력
-    // 단, 학점은 수우미양가 중 하나가 출력되어야 함
+    // 학점은 수우미양가 중 하나가 출력되도록 함
     // 단, 메서드로 기능을 모듈화 시켜 작성
 
     public static void readSungJuk(
@@ -124,16 +117,13 @@ public class J10Method {
         for (int i = 0; i < names.length; i++) {
             System.out.print((i + 1) + "번 학생 이름은? ");
             names[i] = sc.next();
-
             System.out.print((i + 1) + "번 학생 국어는? ");
             kors[i] = sc.nextInt();
-
             System.out.print((i + 1) + "번 학생 영어는? ");
             engs[i] = sc.nextInt();
-
             System.out.print((i + 1) + "번 학생 수학은? ");
             mats[i] = sc.nextInt();
-        }   // 성적 데이터 입력
+        } // 성적 데이터 입력
     }
 
     public static void computeSungJuk(
@@ -144,42 +134,31 @@ public class J10Method {
             tots[i] = kors[i] + engs[i] + mats[i];
             avgs[i] = tots[i] / 3;
 
-            switch ((int) (avgs[i] / 10)) { // fixed variable name
+            switch ((int)(avgs[i]/10)) {
                 case 10:
-                case 9:
-                    grds[i] = '수';
-                    break;
-                case 8:
-                    grds[i] = '우';
-                    break;
-                case 7:
-                    grds[i] = '미';
-                    break;
-                case 6:
-                    grds[i] = '양';
-                    break;
-                default:
-                    grds[i] = '가';
-                    break;
+                case 9: grds[i] = '수'; break;
+                case 8: grds[i] = '우'; break;
+                case 7: grds[i] = '미'; break;
+                case 6: grds[i] = '양'; break;
+                default: grds[i] = '가'; break;
             }
-        }   // 성적 처리
+        }
     }
-
 
     public static void printSungJuk(
             String[] names, int[] kors, int[] engs, int[] mats,
             int[] tots, double[] avgs, char[] grds) {
+
         String result = "";
         for (int i = 0; i < names.length; i++) {
             result += String.format("%s %d %d %d \n",
                     names[i], kors[i], engs[i], mats[i]);
             result += String.format("%d %.1f %c \n",
                     tots[i], avgs[i], grds[i]);
-        }   // 결과 출력 준비
+        } // 결과 출력 준비
 
         System.out.println(result);
     }
+
+
 }
-
-
-
--- src/main/java/seung/J12OOP.java
+++ src/main/java/seung/J12OOP.java
@@ -1,8 +1,20 @@
 package seung;
 
 public class J12OOP {
+    // 클래스 종류
+    // VO/DTO : ValueObject, Data Transfer Object
+    // 계층간 데이터교환을 위한 자바빈즈를 의미
+    // 값만 저장하기 위해 생성하는 클래스
+    // 주로 데이터베이스 테이블의 각 열과 연계해서 작성
+
     public static void main(String[] args) {
         Person2 p1 = new Person2();
+
+        // 객체 멤버변수 초기화 - 실패 - private 때문
+        //p1.name = "혜교";
+        //p1.job = "학생";
+        //p1.age = 35;
+        //p1.gender = '여';
 
         // setter로 변수 초기화
         p1.setName("혜교");
@@ -10,7 +22,7 @@ public class J12OOP {
         p1.setAge(35);
         p1.setGender('여');
 
-        // 객체 출력
+        // 객체 출력 1
         System.out.println(p1.getName());
         System.out.println(p1.getJob());
         System.out.println(p1.getAge());
@@ -21,16 +33,16 @@ public class J12OOP {
 
     }
 }
+
 class Person2 {
     // 멤버변수 캡슐화
-    // 접근 제한자를 이용해서 멤버변수의 직접 접근을 막음
+    // 접근제한자를 이용해서 멤버변수의 직접 접근을 막음
     private String name;
     private String job;
     private int age;
     private char gender;
 
-    public Person2() {
-    } // 기본생성자
+    public Person2() { }
 
     // getter / setter 선언
     // private으로 선언된 변수를 외부에 접근할 수 있도록
@@ -42,7 +54,7 @@ class Person2 {
     }
 
     public String getJob() {
-        return  job;
+        return job;
     }
 
     public int getAge() {
@@ -57,12 +69,15 @@ class Person2 {
     public void setName(String name) {
         this.name = name;
     }
+
     public void setJob(String job) {
         this.job = job;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public void setGender(char gender) {
         this.gender = gender;
     }
