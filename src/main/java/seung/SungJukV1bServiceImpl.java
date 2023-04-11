4e708513a5ea00426a241a4d7f9b8862556ad392
limseungchae <tmdco12@naver.com>
2023-04-11 17:45:46 +0900

    성적처리 프로그램 v1b - 성적수정

--- src/main/java/seung/SungJukV1bServiceImpl.java
+++ src/main/java/seung/SungJukV1bServiceImpl.java
@@ -37,54 +37,76 @@ public class SungJukV1bServiceImpl implements SungJukV1bService {
             case "1": newSungJuk(); break;
             case "2": readSungJuk(); break;
             case "3": readOneSungJuk(); break;
-            case "4": removeSungJuk(); break;
-            case "5": modifySungJuk(); break;
+            case "4": modifySungJuk(); break;
+            case "5": removeSungJuk(); break;
             case "0": System.exit(0); break;
             default:
                 System.out.println("\n>> 잘못 입력하셨습니다! <<\n");
         }
     }
 
-    public void modifySungJuk() {
+    public void removeSungJuk() {
         // 이름입력 -> 대상검색 -> 대상제거
-        System.out.println("삭제할 학생이름은? ");
+        System.out.print("삭제할 학생이름은? ");
         String name = sc.next();
 
         for (int i = 0; i < sjs.length; i++) {
             if (sjs[i] != null && sjs[i].getName().equals(name)) {
-                sjs[i] = null; // 삭제할 배열요소에 null 대입
-                System.out.println("\n삭제되었습니다!\n");
+                sjs[i] = null;  // 삭제할 배열요소에 null 대입
+                System.out.println("\n삭제되었습니다\n");
+                break;
+            }
+        }
+
+    }
+
+    public void modifySungJuk() {
+        // 이름입력 -> 대상검색 -> 새로운데이터입력 -> 성적처리
+        System.out.print("수정할 학생이름은? ");
+        String name = sc.next();
+
+        for (int i = 0; i < sjs.length; i++) {
+            if (sjs[i] != null && sjs[i].getName().equals(name)) {
+                System.out.print("국어는? ");
+                int kor = sc.nextInt();
+                System.out.print("영어은? ");
+                int eng = sc.nextInt();
+                System.out.print("수학은? ");
+                int mat = sc.nextInt();
+
+                SungJukVO sj = new SungJukVO(name, kor, eng, mat);
+                computeSungJuk(sj);
+                sjs[i] = sj;   // 기존 성적데이터 위치에
+                // 새롭게 생성한 객체 대입
+                System.out.println("\n수정완료!!\n");
                 break;
             }
         }
     }
 
-    public void removeSungJuk() {
-        
-    }
-
     public void readOneSungJuk() {
-        // 이름 입력 -> 대상검색 -> 대상출력
-        System.out.println("조회할 학생이름은? ");
+        // 이름입력 -> 대상검색 -> 대상출력
+        System.out.print("조회할 학생이름은? ");
         String name = sc.next();
 
         SungJukVO one = null;
         for (SungJukVO sj : sjs) {
             if (sj != null && sj.getName().equals(name)) {
-                one = sj; break;
+                one = sj;  break;
             }
         }
 
         if (one != null) {
-            System.out.println(one);
+            System.out.printf("\n %s \n\n", one);
         } else {
             System.out.println("\n찾는 데이터가 없습니다!\n");
         }
+
     }
 
     // 성적 리스트 조회 (이름,국어,영어,수학)
     public void readSungJuk() {
-        String fmt = " %s %d %d %d\n";
+        String fmt = "\n %s %d %d %d \n\n";
         for (SungJukVO sj : sjs) {
             if (sj != null)  // 배열에 성적데이터가 존재한다면
                 System.out.printf(fmt, sj.getName(),
