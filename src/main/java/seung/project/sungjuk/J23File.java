3024ad283e16f084b54c150829abd5f04b0974da
limseungchae <tmdco12@naver.com>
2023-04-13 15:44:34 +0900

    파일입출력 finally 예외처리 블럭 추가

--- src/main/java/seung/project/sungjuk/J23File.java
+++ src/main/java/seung/project/sungjuk/J23File.java
@@ -41,24 +41,29 @@ public class J23File {
         String fname = "C:/Java/info.txt";
 
         // 파일쓰기(문자 출력스트림) 객체 생성
+        FileWriter fw = null;
         try {
-            FileWriter fw = new FileWriter(fname);
+            fw = new FileWriter(fname);
 
             // 파일에 내용 쓰기 : write(내용)
             fw.write(name + "\n");
             fw.write(height + "\n");
             fw.write(age + "\n");
 
-            // 파일쓰기가 끝나면 작업 종료
-            fw.close();
+
         } catch (Exception ex) {
             ex.getMessage();
+        } finally {
+            // 파일쓰기가 끝나면 작업 종료
+            if(fw != null)
+                try {fw.close(); } catch (Exception ex) {}
         }
 
         // 파일에 저장된 데이터 화면에 출력1 - 문자 하나씩 읽음
         // 파일읽기(문자 입력스트림) 객체 생성
+        FileReader fr = null;
         try {
-            FileReader fr = new FileReader(fname);
+            fr = new FileReader(fname);
 
             int ch = 0;
             // 파일에서 하나의 문자를 읽어옴
@@ -68,26 +73,32 @@ public class J23File {
                 System.out.print((char) ch);
             }
 
-            fr.close();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
+        } finally {
+            if (fr != null)
+                try {fr.close();} catch (Exception ex) {}
         }
 
         // 파일에 저장된 데이터 화면에 출력2 - 한 행씩 읽음
         // BuffereReader를 이용하면 입력속도를 높일수 있음
+        BufferedReader br = null;
         try {
-            FileReader fr = new FileReader(fname);
-            BufferedReader br = new BufferedReader(fr);
+            fr = new FileReader(fname);
+            br = new BufferedReader(fr);
 
             while (br.ready()) {    // 읽어올 데이터가 있는지 확인
                 // 만일, 있다면 한 행씩 읽어서 화면에 출력
                 System.out.println(br.readLine());
             }
 
-            br.close();
-            fr.close();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
+        } finally {
+            if (br != null)
+                try { br.close(); } catch (Exception ex) {}
+            if (fr != null)
+                try { fr.close(); } catch (Exception ex) {}
         }
 
     }
