4dac0e81550385313297fbf96b3bc659ef74045b
limseungchae <tmdco12@naver.com>
2023-04-14 17:12:05 +0900

    도서정보 select

--- src/main/java/seung/J28JDBC.java
+++ src/main/java/seung/J28JDBC.java
@@ -1,21 +1,23 @@
 package seung;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class J28JDBC {
     private static String DRV = "org.mariadb.jdbc.Driver";
-    private static String URL = "jdbc:mariadb://fullstacks.cmsyuhhb6fnz.ap-northeast-2.rds.amazonaws.com:3306/fullstacks";
+    private static String URL = "jdbc:mariadb://fullstacks.chfcmpaskysu.ap-northeast-2.rds.amazonaws.com:3306/fullstacks";
     private static String USR = "admin";
     private static String PWD = "fullstack_2023";
 
-    private static String selectBookSQL = "select = from newbooks order by bookno desc";
+    private static String selectBookSQL = "select * from newbooks order by bookno desc";
 
     public static void main(String[] args) {
         // newbooks 테이블의 모든 레코드 조회
+        List<Book> bookdata = new ArrayList<>();
 
         try {
-            // 라이브러리 등록
             Class.forName(DRV);
         } catch (ClassNotFoundException e) {
             System.out.println("mariadb 용 JDBC 드라이버가 없어요!!");
@@ -26,27 +28,30 @@ public class J28JDBC {
         ResultSet rs = null;
 
         try {
-            // 데이터베이스 접속
             conn = DriverManager.getConnection(URL, USR, PWD);
             pstmt = conn.prepareStatement(selectBookSQL);
 
             // SQL문 실행 후 결과집합 받음
-            rs = pstmt.executeQuery();    // DML 실행시 사용
+            rs = pstmt.executeQuery();  // DML 실행시 사용
 
-            while (rs.next()) {
-                System.out.println(rs.getInt("bookno") + " ");
-                System.out.println(rs.getString("title") + " ");
-                System.out.println(rs.getString("writer") + " \n");
-
+            while(rs.next()) {
+                Book book = new Book(rs.getInt(1),
+                        rs.getString(2), rs.getString(3),
+                        rs.getInt(4), rs.getString(5));
+                bookdata.add(book);
             }
 
-
         } catch (SQLException e) {
-            System.out.println("디비 접속주소나 아이디/비번을 확인하세요!!");
+            System.out.println("디비 접속주소나 아이디/비번, SQL문을 확인하세요!!");
         } finally {
-            if (rs != null) try { rs.close(); }catch (Exception ex) {}
-            if (pstmt != null) try { pstmt.close(); }catch (Exception ex) {}
-            if (conn != null) try { conn.close(); }catch (Exception ex) {}
+            if (rs != null) try { rs.close(); } catch (Exception ex) {}
+            if (pstmt != null) try { pstmt.close(); } catch (Exception ex) {}
+            if (conn != null) try { conn.close(); } catch (Exception ex) {}
+        }
+
+        // 도서정보 출력
+        for (Book b : bookdata) {
+            System.out.println(b);
         }
 
     }
@@ -55,14 +60,17 @@ public class J28JDBC {
 class Book {
     private int bookno;
     private String title;
-    private String author;
+    private String writer;
     private int price;
     private String regdate;
 
-    public Book(int bookno, String title, String author, int price, String regdate) {
+    public Book() {
+    }
+
+    public Book(int bookno, String title, String writer, int price, String regdate) {
         this.bookno = bookno;
         this.title = title;
-        this.author = author;
+        this.writer = writer;
         this.price = price;
         this.regdate = regdate;
     }
@@ -83,12 +91,12 @@ class Book {
         this.title = title;
     }
 
-    public String getAuthor() {
-        return author;
+    public String getWriter() {
+        return writer;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setWriter(String writer) {
+        this.writer = writer;
     }
 
     public int getPrice() {
@@ -105,5 +113,11 @@ class Book {
 
     public void setRegdate(String regdate) {
         this.regdate = regdate;
+    }
+
+    @Override
+    public String toString() {
+        String fmt = "%d %s %s %d %s";
+        return String.format(fmt, bookno, title, writer, price, regdate);
     }
 }
