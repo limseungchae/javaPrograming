d683440d2a34e866536d187435e2df513f791e01
limseungchae <tmdco12@naver.com>
2023-04-14 11:21:25 +0900

    성적처리 프로그램 v3 - 성적 조회 및 상세조회 loadSungJuk

--- src/main/java/seung/project/sungjuk/dao/SungJukV3DAO.java
+++ src/main/java/seung/project/sungjuk/dao/SungJukV3DAO.java
@@ -2,7 +2,11 @@ package seung.project.sungjuk.dao;
 
 import seung.project.sungjuk.model.SungJukVO;
 
+import java.util.List;
+
 public interface SungJukV3DAO {
 
     boolean saveSungJuk(SungJukVO sj);
+
+    List<SungJukVO> lodSungJuk();
 }
